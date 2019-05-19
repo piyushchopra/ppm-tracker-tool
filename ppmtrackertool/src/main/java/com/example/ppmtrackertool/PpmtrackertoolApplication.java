882b877f439be446042ce9246f99296141be209c
piyush.chopra <piyush.chopra@payu.in>
2019-05-19 21:37:55 +0530

    first commit

---
+++ ppmtrackertool/src/main/java/com/example/ppmtrackertool/PpmtrackertoolApplication.java
@@ -0,0 +1,13 @@
+package com.example.ppmtrackertool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmtrackertoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpmtrackertoolApplication.class, args);
+	}
+
+}
