31e288336eb4cc2ed000c2fd7329f3220bb6ad53
piyush.chopra <piyush.chopra@payu.in>
2019-05-30 10:46:27 +0530

    Binding result validation added

--- ppmtrackertool/src/main/java/com/example/ppmtrackertool/web/ProjectController.java
+++ ppmtrackertool/src/main/java/com/example/ppmtrackertool/web/ProjectController.java
@@ -5,7 +5,10 @@ import com.example.ppmtrackertool.domain.Project;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/project")
@@ -15,8 +18,13 @@ public class ProjectController {
     ProjectService projectService;
 
     @PostMapping("")
-    public ResponseEntity<Project> createNewProject(@RequestBody Project project) {
-        projectService.saveOrUpdate(project);
+    public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+            return new ResponseEntity<String>("Invalid Project Object", HttpStatus.BAD_REQUEST);
+        }
+
+        Project project1 = projectService.saveOrUpdate(project);
         return new ResponseEntity<Project>(project, HttpStatus.ACCEPTED);
     }
 
