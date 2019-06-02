30a6bcc82a4c9bf3f4fc7a78d22d2939e0e0bd51
piyush.chopra <piyush.chopra@payu.in>
2019-06-02 15:09:47 +0530

    error field map returned

--- ppmtrackertool/src/main/java/com/example/ppmtrackertool/web/ProjectController.java
+++ ppmtrackertool/src/main/java/com/example/ppmtrackertool/web/ProjectController.java
@@ -6,9 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/project")
@@ -18,14 +22,17 @@ public class ProjectController {
     ProjectService projectService;
 
     @PostMapping("")
-    public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult bindingResult) {
+    public ResponseEntity<?> createNewProject( @Valid @RequestBody Project project, BindingResult bindingResult) {
 
         if (bindingResult.hasErrors()) {
-            return new ResponseEntity<String>("Invalid Project Object", HttpStatus.BAD_REQUEST);
+            Map<String, String> errorMap = new HashMap<>();
+            //pupose is to return error fields in name-value pair
+            // all the field errors are present in the list reurned by fieldError.getField()
+            bindingResult.getFieldErrors().forEach(fieldError -> errorMap.put(fieldError.getField(), fieldError.getDefaultMessage()));
+            return new ResponseEntity<Map<String, String>>(errorMap, HttpStatus.BAD_REQUEST);
         }
-
         Project project1 = projectService.saveOrUpdate(project);
-        return new ResponseEntity<Project>(project, HttpStatus.ACCEPTED);
+        return new ResponseEntity<Project>(project, HttpStatus.CREATED);
     }
 
     @GetMapping("/test")
