049b42347a9873d4d0a043188dea1c595bbb0b81
piyush.chopra <piyush.chopra@payu.in>
2019-06-10 23:45:24 +0530

    proj id checked in db before saving

--- ppmtrackertool/src/main/java/com/example/ppmtrackertool/services/ProjectService.java
+++ ppmtrackertool/src/main/java/com/example/ppmtrackertool/services/ProjectService.java
@@ -1,6 +1,7 @@
 package com.example.ppmtrackertool.services;
 
 import com.example.ppmtrackertool.domain.Project;
+import com.example.ppmtrackertool.exception.ProjectIdException;
 import com.example.ppmtrackertool.repository.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,6 +13,12 @@ public class ProjectService {
     private ProjectRepository projectRepository;
 
     public Project saveOrUpdate(Project project) {
-        return projectRepository.save(project);
+
+        try {
+            project.setProjectIdentifier(project.getProjectIdentifier());
+            return projectRepository.save(project);
+        } catch (Exception e) {
+            throw new ProjectIdException("Project ID '"+project.getProjectIdentifier().toUpperCase()+"' already exists");
+        }
     }
 }
